68dfabbe736016f563004e275c584f15503e9319
tringuyen <tri.nh@hotmail.com>
2022-04-11 15:49:17 +0700

    get product info

--- main.js
+++ main.js
@@ -752,7 +752,7 @@ app.post('/getsanpham/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('SELECT * FROM sanphamtonkho where id = ($1)', postData, function (error, results, fields) {
+    pool.query('SELECT * FROM public.product INNER JOIN product_storage ON product.id = product_storage.product_id where product.id= where id = ($1)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
