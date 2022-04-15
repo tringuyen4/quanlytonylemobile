0abbac083f7bfb87d5cf22d92556ef94aa8be23f
tringuyen <tri.nh@hotmail.com>
2022-04-15 14:24:06 +0700

    up

--- main.js
+++ main.js
@@ -648,7 +648,7 @@ app.post('/danhsachsanphamdaban/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('INSERT INTO danhsachsanphamdaban VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)', postData, function (error, results, fields) {
+    pool.query('INSERT INTO danhsachsanphamdaban VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
