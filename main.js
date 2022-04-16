a8a5530c1c21bfc56ed345d1081d2a50122d5fd1
tringuyen <tri.nh@hotmail.com>
2022-04-16 10:08:49 +0700

    up

--- main.js
+++ main.js
@@ -641,6 +641,32 @@ app.get('/getdanhsachsanphamdabanquanlymobile/', function (req, res) {
     });
 });
 
+app.get('/getdanhsachsanphamdabanquanlymobilejp/', function (req, res) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    console.log(req);
+    pool.query(`select * from danhsachsanphamdaban where vitri='SHOP_JP'`, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
+app.get('/getdanhsachsanphamdabanquanlymobilevn/', function (req, res) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    console.log(req);
+    pool.query(`select * from danhsachsanphamdaban where vitri='SHOP_VN'`, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 app.post('/danhsachsanphamdaban/', function (req, res) {
     var postData = req.body;
     res.header("Access-Control-Allow-Origin", "*");
