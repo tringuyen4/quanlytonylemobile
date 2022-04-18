bb19164d0182ed39eeb8b0bfdb9ac0244f66d656
tringuyen <tri.nh@hotmail.com>
2022-04-18 11:36:42 +0700

    update

--- main.js
+++ main.js
@@ -635,7 +635,7 @@ app.get('/getdanhsachsanphamdabanquanlymobile/', function (req, res) {
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query('select * from danhsachsanphamdaban', function (error, results, fields) {
+    pool.query(`select * from danhsachsanphamdaban where vitri='WAREHOUSE'`, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
