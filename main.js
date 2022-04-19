b953e80943081ea44dc4100e9233c5cc462c9468
tringuyen <tri.nh@hotmail.com>
2022-04-19 16:12:02 +0700

    up

--- main.js
+++ main.js
@@ -596,7 +596,7 @@ app.get('/getdanhsachdonhangquanlymobileall/', function (req, res) {
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query(`select * from danhsachdonhang`, function (error, results, fields) {
+    pool.query('select * from danhsachdonhang', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
