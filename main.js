5cd7078f6217f3575f4c8ce3d8620e967deecb29
tringuyen <tri.nh@hotmail.com>
2022-05-21 11:15:14 +0700

    up

--- main.js
+++ main.js
@@ -842,6 +842,19 @@ app.post('/deletequanlythu/', function (req, res) {
     });
 });
 
+app.post('/deletequanlythutransactionkey/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('DELETE FROM quanlythu where mucdich = ($1)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 app.post('/deletequanlychi/', function (req, res) {
     var postData = req.body;
     res.header("Access-Control-Allow-Origin", "*");
