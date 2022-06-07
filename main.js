459a224aa731abb760ee1f33b7b604bc8c4eb630
tringuyen <tri.nh@hotmail.com>
2022-06-07 11:15:01 +0700

    up

--- main.js
+++ main.js
@@ -1161,6 +1161,19 @@ app.put('/updatetrangthaidonhang/', function (req, res) {
     });
 });
 
+app.put('/updatetrangthaidonhangdatcoc/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('UPDATE danhsachdonhang SET trangthaidonhang = ($1), tienmat = ($2), daikibi = ($3), chuyenkhoan = ($4) where madonhang = ($5)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 app.post('/deletedanhsachdonhang/', function (req, res) {
     var postData = req.body;
     res.header("Access-Control-Allow-Origin", "*");
