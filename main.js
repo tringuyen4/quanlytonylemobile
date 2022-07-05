2a0f9f35da82452a745b4351d4cf3c3960b4b6c5
tringuyen <tri.nh@hotmail.com>
2022-07-05 20:35:07 +0700

    up

--- main.js
+++ main.js
@@ -1043,6 +1043,19 @@ app.post('/getsanpham/', function (req, res) {
     });
 });
 
+app.post('/getsanphamemei/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('SELECT * FROM product INNER JOIN product_storage ON product.id = product_storage.product_id where product.imei = ($1)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 app.post('/updatesanpham/', function (req, res) {
     var postData = req.body;
     res.header("Access-Control-Allow-Origin", "*");
