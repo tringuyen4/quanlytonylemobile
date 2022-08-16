cff58b7de97027d0ccc102ddbf63e50a3c155ceb
Tri Nguyen <tri.nguyen1@tyme.com>
2022-08-16 23:49:38 +0700

    update

--- services/customer.service.js
+++ services/customer.service.js
@@ -1,7 +1,7 @@
-const {DATA_TABLES} = require("../constants/data.constant");
-const {HTTP_STATUSES} = require("../constants/http.constant");
-const {notEmpty} = require("../utils/data.utils");
-const {INVOICE_STATUS} = require("../constants/common.constant")
+const { DATA_TABLES } = require("../constants/data.constant");
+const { HTTP_STATUSES } = require("../constants/http.constant");
+const { notEmpty } = require("../utils/data.utils");
+const { INVOICE_STATUS } = require("../constants/common.constant")
 
 class CustomerService {
     constructor(dbPool) {
@@ -20,11 +20,16 @@ class CustomerService {
                                          age,
                                          address,
                                          phone,
-                                         job
+                                         job,
+                                         payment_method,
+                                         bank_name,
+                                         branch_name,
+                                         bank_id,
+                                         account_name
                                   FROM ${DATA_TABLES.CUSTOMER};`
 
         return this.pool.query(getCustomerQuery)
-            .then(({rows}) => {
+            .then(({ rows }) => {
                 return rows
             }).catch(e => {
                 throw e;
@@ -39,12 +44,12 @@ class CustomerService {
      */
     addCustomer(customerData) {
 
-        const insertCustomerSql = `INSERT INTO ${DATA_TABLES.CUSTOMER} (name_vietnamese, name_japanese, birthday, age, address, phone, job)
-                                   VALUES ($1, $2, $3, $4, $5, $6, $7)
+        const insertCustomerSql = `INSERT INTO ${DATA_TABLES.CUSTOMER} (name_vietnamese, name_japanese, birthday, age, address, phone, job, payment_method, bank_name, branch_name, bank_id, account_name)
+                                   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
                                    RETURNING *;`;
 
         return this.pool.query(insertCustomerSql, Object.values(customerData))
-            .then(({rows}) => {
+            .then(({ rows }) => {
                 if (rows.length > 0) {
                     return rows[0]
                 }
@@ -70,12 +75,17 @@ class CustomerService {
                                          age=$4,
                                          address=$5,
                                          phone=$6,
-                                         job=$7
+                                         job=$7,
+                                         payment_method=$8,
+                                         bank_name=$9,
+                                         branch_name=$10,
+                                         bank_id=$11,
+                                         account_name=$12
                                      WHERE id = ${customerId}
                                      RETURNING *;`;
 
         return this.pool.query(updateCustomerQuery, Object.values(customerData))
-            .then(({rows}) => {
+            .then(({ rows }) => {
                 return (rows.length > 0) ? rows[0] : null
             })
             .catch(e => {
@@ -90,24 +100,24 @@ class CustomerService {
                                     where c.id = pd.customer_id and id.invoice_id = pd.invoice_id and c.id = $1
                                     `;
         return this.pool.query(customerProductQuery, [customerId])
-                    .then(({rows}) => {
-                        const promises = [];
-                        rows.forEach((product) => {
-                            const {product_id, quantity} = product;
-                            promises.push(this.pool.query(`update ${DATA_TABLES.PRODUCT} set quantity= quantity - $2 where id = $1`, [product_id, quantity]))
-                        });
-                        return Promise.all(promises)
+            .then(({ rows }) => {
+                const promises = [];
+                rows.forEach((product) => {
+                    const { product_id, quantity } = product;
+                    promises.push(this.pool.query(`update ${DATA_TABLES.PRODUCT} set quantity= quantity - $2 where id = $1`, [product_id, quantity]))
+                });
+                return Promise.all(promises)
+                    .then(() => {
+                        return this.pool.query(`update ${DATA_TABLES.INVOICE} set status = '${INVOICE_STATUS.TERMINATED}' where id in (select invoice_id from ${DATA_TABLES.PURCHASING_DETAIL} where customer_id = $1)`, [customerId])
+                            .then(() => {
+                                return this.pool.query(`delete from ${DATA_TABLES.CUSTOMER} where id = ${customerId}`)
                                     .then(() => {
-                                        return this.pool.query(`update ${DATA_TABLES.INVOICE} set status = '${INVOICE_STATUS.TERMINATED}' where id in (select invoice_id from ${DATA_TABLES.PURCHASING_DETAIL} where customer_id = $1)`, [customerId])
-                                                    .then(() => {
-                                                        return this.pool.query(`delete from ${DATA_TABLES.CUSTOMER} where id = ${customerId}`)
-                                                                    .then(() => {
-                                                                        return true;
-                                                                    });
-                                                    });
+                                        return true;
                                     });
+                            });
+                    });
 
-                    })
+            })
     }
 
     searchCustomer(search_type = 'BIRTHDAY', query = null) {
@@ -124,7 +134,12 @@ class CustomerService {
                                        age,
                                        address,
                                        phone,
-                                       job
+                                       job,
+                                       payment_method,
+                                         bank_name,
+                                         branch_name,
+                                         bank_id,
+                                         account_name
                                 FROM ${DATA_TABLES.CUSTOMER}
                                 WHERE birthday = '${query.birthday}'`;
                     break;
@@ -132,7 +147,7 @@ class CustomerService {
 
             if (notEmpty(queryStr)) {
                 return this.pool.query(queryStr)
-                    .then(({rows}) => {
+                    .then(({ rows }) => {
                         return rows.length > 0 ? rows : null;
                     })
                     .catch(e => {
@@ -142,7 +157,7 @@ class CustomerService {
                 return Promise.resolve(null);
             }
         } else {
-            return Promise.reject({error: 'Empty search type or query.'});
+            return Promise.reject({ error: 'Empty search type or query.' });
         }
     }
 }
